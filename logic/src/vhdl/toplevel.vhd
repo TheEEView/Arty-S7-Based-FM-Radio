ee862e947df5e4af4b176929c85eb603eaa94599
Glenn Kirilow <127254114+TheEEView@users.noreply.github.com>
2026-03-13 06:56:34 +1100

    Add volume up/down and channel inc/dec buttons and use inverted mmcm lock signal for power on reset until mmcm is stable

--- logic/src/vhdl/toplevel.vhd
+++ logic/src/vhdl/toplevel.vhd
@@ -5,8 +5,10 @@ use ieee.numeric_std.all;
 entity toplevel is
 port (
     i_clk_12        : in std_logic;     --! 12 MHz clock input to FPGA
-    i_rst           : in std_logic;     --! Active high reset input to FPGA
-    -- TODO: Add volume up/down and channel selection button inputs
+    i_btn_vol_up    : in std_logic;     --! Active high volume up button
+    i_btn_vol_down  : in std_logic;     --! Active high volume down button
+    i_btn_ch_up     : in std_logic;     --! Active high channel up button
+    i_btn_ch_down   : in std_logic;     --! Active high channel down button
     o_adc_sdi       : out std_logic;    --! MCP33131 ADC SDI output to FPGA
     o_adc_sclk      : out std_logic;    --! MCP33131 ADC SCLK output to FPGA
     i_adc_sdo       : in std_logic;     --! MCP33131 ADC SDO input from FPGA
@@ -16,29 +18,71 @@ port (
 end entity;
 
 architecture rtl of toplevel is
+constant SYSCLK_FREQ_HZ     : natural := 60000000; --! System clock frequency in Hz for debounce and ADC/VCO drivers
+constant DEB_CNT_50MS       : natural := (50*SYSCLK_FREQ_HZ) / 1000; --! Number of clock cycles the button needs to remain stable without switch bouncing
+
 signal clk_60               : std_logic; -- We use a 60 MHz clock for the 30 MHz ADC SCLK generation
 signal mmcm_lock            : std_logic; -- MMCM lock signal to indicate stable clock output also used for reset button debounce reset input
-signal inverted_mmcm_lock   : std_logic;
+signal mmcm_reset           : std_logic;
+
+signal vol_up_pulse         : std_logic;
+signal vol_down_pulse       : std_logic;
+signal ch_up_pulse          : std_logic;
+signal ch_down_pulse        : std_logic;
 begin
 
 -- TODO: Add MMCM here to generate 60 MHz clock from 12 MHz input clock
 
-inverted_mmcm_lock <= not mmcm_lock;
+-- Use inverted mmcm lock signal as a power on reset before the MMCM is ready
+mmcm_reset <= not mmcm_lock;
 
--- Debounce reset button synchronized to 60 MHz sysclk to generate a 1 clock cycle high pulse for fan out to all reset inputs
-i_rst_btn_deb : btn_deb
+i_btn_vol_up_deb : btn_deb
 generic map (
-    DEB_CNT         => 1000000,
+    DEB_CNT         => DEB_CNT_50MS,
     ACTIVE_HIGH_BTN => true
 )
 port map (
     i_sysclk        => clk_60,
-    i_rst           => inverted_mmcm_lock, -- This will be in reset before sys clk is ready
-    i_btn           => i_rst_btn_deb.o_pulse,
-    o_pulse         => rst_pulse
+    i_rst           => mmcm_reset,
+    i_btn           => i_btn_vol_up,
+    o_pulse         => vol_up_pulse
 );
 
--- TODO: Add debouncing for other buttons here
+i_btn_vol_down_deb : btn_deb
+generic map (
+    DEB_CNT         => DEB_CNT_50MS,
+    ACTIVE_HIGH_BTN => true
+)
+port map (
+    i_sysclk        => clk_60,
+    i_rst           => mmcm_reset,
+    i_btn           => i_btn_vol_down,
+    o_pulse         => vol_down_pulse
+);
+
+i_btn_ch_up_deb : btn_deb
+generic map (
+    DEB_CNT         => DEB_CNT_50MS,
+    ACTIVE_HIGH_BTN => true
+)
+port map (
+    i_sysclk        => clk_60,
+    i_rst           => mmcm_reset,
+    i_btn           => i_btn_ch_up,
+    o_pulse         => ch_up_pulse
+);
+
+i_btn_ch_down_deb : btn_deb
+generic map (
+    DEB_CNT         => DEB_CNT_50MS,
+    ACTIVE_HIGH_BTN => true
+)
+port map (
+    i_sysclk        => clk_60,
+    i_rst           => mmcm_reset,
+    i_btn           => i_btn_ch_down,
+    o_pulse         => ch_down_pulse
+);
 
 i_mpc33131_adc_driver : entity work.mpc33131_adc_driver
 port map (
